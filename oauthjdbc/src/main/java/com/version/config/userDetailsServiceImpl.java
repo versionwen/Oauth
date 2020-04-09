2ad052969e1bf9773b82472b6bc7dce370650a03
versionwen <49876178+versionwen@users.noreply.github.com>
2020-04-09 23:59:30 +0800

    修改为基于RBAC模型的认证与授权，bug已经修复

--- oauthjdbc/src/main/java/com/version/config/userDetailsServiceImpl.java
+++ oauthjdbc/src/main/java/com/version/config/userDetailsServiceImpl.java
@@ -13,10 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * @author version
@@ -27,133 +24,33 @@ import java.util.ListIterator;
 public class userDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private TbUserService tbUserService;
+
     @Autowired
     private TbPermissionService tbPermissionService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        TbUser tbUser=tbUserService.quertByname(username);
-        List<GrantedAuthority>grantedAuthorities=new List<GrantedAuthority>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<GrantedAuthority> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(GrantedAuthority grantedAuthority) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends GrantedAuthority> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends GrantedAuthority> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public GrantedAuthority get(int index) {
-                return null;
-            }
-
-            @Override
-            public GrantedAuthority set(int index, GrantedAuthority element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, GrantedAuthority element) {
-
-            }
-
-            @Override
-            public GrantedAuthority remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<GrantedAuthority> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<GrantedAuthority> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<GrantedAuthority> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
-        if(tbUser!=null){
-            List<TbPermission>tbPermissions=tbPermissionService.queryAllPermisson(tbUser.getId());
-            tbPermissions.forEach(tbPermission -> {GrantedAuthority grantedAuthority=new SimpleGrantedAuthority(tbPermission.getEnname());
-            grantedAuthorities.add(grantedAuthority);
+        System.out.println("-----------"+username);
+        // 查询用户信息
+        TbUser tbUser = tbUserService.quertByname(username);
+        System.out.println("##############");
+        System.out.println("**********"+tbUser.getUsername());
+        System.out.println("-----------------");
+        System.out.println();
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+        if (tbUser != null) {
+            // 获取用户授权
+            List<TbPermission> tbPermissions = tbPermissionService.queryAllPermisson(tbUser.getId());
+            // 声明用户授权
+            tbPermissions.forEach(tbPermission -> {
+                if (tbPermission != null && tbPermission.getEnname() != null) {
+                    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(tbPermission.getEnname());
+                    grantedAuthorities.add(grantedAuthority);
+                }
             });
         }
-        return new User(tbUser.getUsername(),tbUser.getPassword(),grantedAuthorities);
+
+        // 由框架完成认证工作
+        return new User(tbUser.getUsername(), tbUser.getPassword(), grantedAuthorities);
     }
 }
